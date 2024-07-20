21cd8f38c8a2acbc35f831fdcdb2c30851fca3d8
pr19gya <pragya.shri2001@gmail.com>
2024-07-21 00:38:21 +0530

    .

--- src/components/projects/HeroSection.jsx
+++ src/components/projects/HeroSection.jsx
@@ -5,14 +5,14 @@ import { motion } from 'framer-motion';
 const HeroSection = () => {
   return (
     <div className='relative h-[650px] w-full'>
-      <img src="/miquel-parera-VGmgsDsck58-unsplash.jpg" className='h-full w-full object-cover' alt="Background"/>
+      <img src="/1712478256146.jpg" className='h-full w-full object-cover' alt="Background"/>
 
       {/* Overlay Content */}
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className='absolute inset-0 flex flex-col justify-center items-center text-center px-6 md:px-10 lg:px-20'
+        className='absolute inset-0 flex flex-col bg-black bg-opacity-40 justify-center items-center text-center px-6 md:px-10 lg:px-20'
       >
       <div className='absolute inset-0 flex flex-col justify-center items-center text-center px-6 md:px-10 lg:px-20'>
         <h1 className='text-white text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold leading-tight md:leading-snug lg:leading-normal xl:leading-relaxed'>
